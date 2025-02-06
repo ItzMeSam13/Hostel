847075ea40698f34f0876790fc6b3f47affdafbb
ItzMeSam <sampreetkalkundrikar8@gmail.com>
2025-02-06 23:09:30 +0530

    My first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+import express from "express"; // to create server
+import authRouter from "./src/routes/AuthRoute.js"; //importing AuthRoutes
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const port = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+	res.send("welcome to API");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
